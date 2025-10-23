7ac3ffb9f55095b0e6bf5f474cfe22ed6d530a23
Muhammad Wajahat Tariq <wajahatariq4@gmail.com>
2025-10-24 00:59:45 +0500

    Update Manager_app.py

--- Manager_app.py
+++ Manager_app.py
@@ -82,12 +82,12 @@ def render_transaction_tabs(df, worksheet, label):
                     first_name = name_parts[0] if len(name_parts) > 0 else ''
                     last_name = ' '.join(name_parts[1:]) if len(name_parts) > 1 else ''
                     
-                    st.write(f"**Card Holder Name:** {card_holder}")
                     st.write(f"**First Name:** {first_name}")
                     st.write(f"**Last Name:** {last_name}")
 
                     st.write(f"**Address:** {row['Address']}")
                     st.write(f"**CVC:** {row['CVC']}")
+                    st.write(f"**Card Holder Name:** {card_holder}")
 
                     row_number = i + 2  # header is row 1
                     col_number = df.columns.get_loc("Status") + 1
